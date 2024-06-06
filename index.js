f3dc163eaa215fe2f7d89ba0d21f3a8f91eec9d7
AetlantisHR <dannyjosuepm@gmail.com>
2024-06-06 12:17:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,36 @@
-const { Highrise, Events } = require("highrise.sdk.dev");
-const port = process.env.PORT || 8080;
-
+const { Highrise, Events, Emotes } = require("highrise.sdk.dev");
+const port = process.env.PORT || 4000;
 const bot = new Highrise({
-  events: [
+  Events: [
     Events.Messages,
-    Events.ChatCreate,
-    Events.PlayerEmote,
-    Events.Emote,
-    Events.Error,
-    Events.Ready
+    Events.Emotes
   ]
 });
-
-bot.on(Events.Ready, () => {
+bot.on("ready", (session) => {
   console.log("The bot is online!");
 });
-
-bot.on(Events.ChatCreate, (user, message) => {
-  if (user.id === bot.info.user.id) return;
-
+bot.on("chatCreate", (user, message) => {
+  if (bot.info.user.id === user.id) return;
   console.log(`${user.username} said: ${message}`);
-
   if (message === "hi") {
-    bot.message.send("hi");
+    return bot.message.send("hi");
   } else if (message === "hello") {
-    bot.whisper.send(user.id, "Hello, this is a whisper.");
+    return bot.whisper.send(user.id, "Hello, this is a whisper.");
   } else if (message === "emote") {
-    bot.player.emote(user.id, "dance-singleladies");
+    return bot.player.emote(user.id, Emotes.dance_singleladies.id);
   }
 });
 
-bot.on(Events.PlayerEmote, (sender, receiver, emote) => {
+bot.on("playerEmote", (sender, receiver, emote) => {
   console.log(`${sender.username} performed an emote on ${receiver.username} "${emote}"`);
 });
-
-bot.on(Events.Emote, (event) => {
-  const { user: { id: userId }, emote: emoteName } = event;
-  console.log(`User with ID ${userId} is using the emote: ${emoteName}`);
+bot.on("emote", (event) => {
+  const userId = event.user.id;
+  const emoteName = event.emote;
+  
+  console.log(`El usuario con ID ${userId} está usando el emote: ${emoteName}`);
 });
-
-bot.on(Events.Error, (error) => {
-  console.error(error);
+bot.on("error", (message) => {
+  console.log(message);
 });
-
 bot.login(process.env.token, process.env.room);
