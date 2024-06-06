02641c9ef08c45b5739fbf756cf01aeff5656c3e
AetlantisHR <dannyjosuepm@gmail.com>
2024-06-06 12:31:28 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -6,31 +6,45 @@ const bot = new Highrise({
     Events.Emotes
   ]
 });
+
+const emoteMappings = {
+  "Tiktok10": Emotes.dance_singleladies.id,
+  "Dance": Emotes.hot.id
+};
+
 bot.on("ready", (session) => {
   console.log("The bot is online!");
 });
+
 bot.on("chatCreate", (user, message) => {
   if (bot.info.user.id === user.id) return;
   console.log(`${user.username} said: ${message}`);
-  if (message === "hi") {
+  
+  const emoteKey = message.toLowerCase();
+  const emoteId = emoteMappings[emoteKey];
+
+  if (emoteId) {
+    return bot.player.emote(user.id, emoteId);
+  } else if (message === "hi") {
     return bot.message.send("hi");
   } else if (message === "hello") {
     return bot.whisper.send(user.id, "Hello, this is a whisper.");
-  } else if (message === "emote") {
-    return bot.player.emote(user.id, Emotes.dance_singleladies.id);
   }
 });
 
 bot.on("playerEmote", (sender, receiver, emote) => {
   console.log(`${sender.username} performed an emote on ${receiver.username} "${emote}"`);
 });
+
 bot.on("emote", (event) => {
   const userId = event.user.id;
   const emoteName = event.emote;
   
   console.log(`El usuario con ID ${userId} está usando el emote: ${emoteName}`);
 });
+
 bot.on("error", (message) => {
   console.log(message);
 });
+
 bot.login(process.env.token, process.env.room);
